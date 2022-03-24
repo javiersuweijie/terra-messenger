4e2956895778ad87f7ab983854543f1c6dd2f416
Javier Su Weijie <javier.su.weijie@gmail.com>
2022-03-24 22:32:26 +0800

    refactor: remove template contract

--- contracts/messenger/src/msg.rs
+++ contracts/messenger/src/msg.rs
@@ -3,24 +3,11 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-  /// count
-  pub count: i32,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-  /// increment count
-  Increment {},
-
-  /// decrement count
-  Decrement {},
-
-  /// reset count (contract owner only)
-  Reset {
-    /// count
-    count: i32,
-  },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -29,13 +16,4 @@ pub struct MigrateMsg {}
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-  /// @return CountResponse
-  GetCount {},
-}
-
-/// Response type of QueryMsg.GetCount
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-  /// count property
-  pub count: i32,
 }
