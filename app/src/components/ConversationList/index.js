7505528011c30fdb6afd269d9753063a782ef029
Javier Su Weijie <javier.su.weijie@gmail.com>
2022-03-27 17:00:04 +0800

    feature: start a new conversation

--- app/src/components/ConversationList/index.js
+++ app/src/components/ConversationList/index.js
@@ -1,30 +1,114 @@
-import React, { useState, useEffect } from 'react';
-import ConversationSearch from '../ConversationSearch';
+import { Fee, LCDClient, MsgExecuteContract } from '@terra-money/terra.js';
+import { default as React, useCallback } from 'react';
+import { useRecoilRefresher_UNSTABLE, useRecoilValue } from 'recoil';
+import { chatsState, selectedChatState } from '../../data/chats';
+import { messagesState } from '../../data/messages';
+import { networkState } from '../../data/networks';
+import { walletState } from '../../data/wallet';
 import ConversationListItem from '../ConversationListItem';
 import Toolbar from '../Toolbar';
 import ToolbarButton from '../ToolbarButton';
-import axios from 'axios';
-
 import './ConversationList.css';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { chatsState } from '../../data/chats';
 
 export default function ConversationList(props) {
   const conversations = useRecoilValue(chatsState);
+  const wallet = useRecoilValue(walletState);
+  const network = useRecoilValue(networkState);
+  const { name } = useRecoilValue(selectedChatState);
+  const resetChatList = useRecoilRefresher_UNSTABLE(chatsState);
+
+  const postTx = useCallback(
+    async (executeMsg, contract) => {
+      if (!wallet || !contract) {
+        console.log(wallet, executeMsg, contract);
+        return;
+      }
+
+      try {
+        const { result } = await wallet.post({
+          msgs: [
+            new MsgExecuteContract(wallet.terraAddress, contract, executeMsg),
+          ],
+          fee: new Fee(1000000, '200000uusd'),
+        });
+
+        const pollResult = await pollTxInfo(wallet.network, result.txhash);
+        console.log(pollResult);
+        resetChatList();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [wallet],
+  );
+
+  const onAdd = useCallback(
+    () => {
+      const name = window.prompt('What is wallet address?');
+      postTx(
+        {
+          send_message: {
+            data: 'Hello! This is the start of our conversation',
+            to: name,
+          },
+        },
+        network.messengerContract,
+      );
+    },
+    [postTx, name],
+  );
 
   return (
     <div className="conversation-list">
       <Toolbar
         title="Messenger"
-        leftItems={[<ToolbarButton key="cog" icon="ion-ios-cog" />]}
         rightItems={[
-          <ToolbarButton key="add" icon="ion-ios-add-circle-outline" />,
+          <ToolbarButton
+            key="add"
+            icon="ion-ios-add-circle-outline"
+            onClick={onAdd}
+          />,
         ]}
       />
-      <ConversationSearch />
       {conversations.map(conversation => (
         <ConversationListItem key={conversation.id} data={conversation} />
       ))}
     </div>
   );
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function pollTxInfo(network, txhash) {
+  const until = Date.now() + 1000 * 60 * 60;
+  const untilInterval = Date.now() + 1000 * 60;
+
+  const lcd = new LCDClient({
+    chainID: network.chainID,
+    URL: network.lcd,
+  });
+
+  while (true) {
+    let txInfo;
+
+    try {
+      txInfo = await lcd.tx.txInfo(txhash);
+    } catch {}
+
+    if (txInfo) {
+      return txInfo;
+    } else if (Date.now() < untilInterval) {
+      await sleep(500);
+    } else if (Date.now() < until) {
+      await sleep(1000 * 10);
+    } else {
+      throw new Error(
+        `Transaction queued. To verify the status, please check the transaction hash below.`,
+      );
+    }
+  }
 }
--- app/src/components/Messenger/index.js
+++ app/src/components/Messenger/index.js
@@ -11,29 +11,11 @@ export default function Messenger(props) {
   useEffect(() => {
     setInterval(() => {
       resetChat();
-    }, 5000);
+    }, 10000);
   });
 
   return (
     <div className="messenger">
-      {/* <Toolbar
-          title="Messenger"
-          leftItems={[
-            <ToolbarButton key="cog" icon="ion-ios-cog" />
-          ]}
-          rightItems={[
-            <ToolbarButton key="add" icon="ion-ios-add-circle-outline" />
-          ]}
-        /> */}
-
-      {/* <Toolbar
-          title="Conversation Title"
-          rightItems={[
-            <ToolbarButton key="info" icon="ion-ios-information-circle-outline" />,
-            <ToolbarButton key="video" icon="ion-ios-videocam" />,
-            <ToolbarButton key="phone" icon="ion-ios-call" />
-          ]}
-        /> */}
 
       <div className="scrollable sidebar">
         <ConversationList />
